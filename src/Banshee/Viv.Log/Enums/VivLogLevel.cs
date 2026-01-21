88cb4b95be4f7236b582c64238836ffb36555500
Leiwanglin <778192960@qq.com>
2026-01-21 14:44:04 +0800

    fix

---
+++ src/Banshee/Viv.Log/Enums/VivLogLevel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Viv.Log.Enums
+{
+    public enum VivLogLevel
+    {
+        Debug,
+        Info,
+        Warn,
+        Error,
+        Fatal
+    }
+}
