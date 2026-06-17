befff7e4c5fa93d5523c606a0a21b3620e38e9f9
Leiwanglin <778192960@qq.com>
2026-06-17 18:02:48 +0800

    fix

--- src/Vivian/Viv.Aspire/Viv.Aspire.ServiceDefaults/Extensions.cs
+++ src/Vivian/Viv.Aspire/Viv.Aspire.ServiceDefaults/Extensions.cs
@@ -46,12 +46,6 @@ public static class Extensions
 
     public static TBuilder ConfigureOpenTelemetry<TBuilder>(this TBuilder builder) where TBuilder : IHostApplicationBuilder
     {
-        builder.Logging.AddOpenTelemetry(logging =>
-        {
-            logging.IncludeFormattedMessage = true;
-            logging.IncludeScopes = true;
-        });
-
         builder.Services.AddOpenTelemetry()
             .WithMetrics(metrics =>
             {
