007c915492dab3576313a49e3a0b57251ec1ced8
Leiwanglin <778192960@qq.com>
2026-06-22 15:44:33 +0800

    fix

--- src/Vivian/Viv.SakuMai.Api/TickerQDbContextFactory.cs
+++ src/Vivian/Viv.SakuMai.Api/TickerQDbContextFactory.cs
@@ -14,7 +14,7 @@ namespace Viv.SakuMai.Api
         /// <returns></returns>
         public TickerQDbContext CreateDbContext(string[] args)
         {
-            string conn = "server=43.228.79.205;user id=sa;password=viv_sqlserver_77;database=viv_tickerq_core;min pool size=4;max pool size=512;TrustServerCertificate=true;";
+            string conn = "";
             var builder = new DbContextOptionsBuilder<TickerQDbContext>();
             builder.UseSqlServer(conn, o => o.MigrationsAssembly("Viv.SakuMai.Api"));
             return new TickerQDbContext(builder.Options);
