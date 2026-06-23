67b91bc8041432a7fd582558727ebf5cc6432993
Leiwanglin <778192960@qq.com>
2026-06-23 15:59:58 +0800

    fix

---
+++ src/Vivian/Viv.Entity/CacheBucket/OneListBucket.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Viv.Delusion.Extension;
+using Viv.Elysia;
+using Viv.Elysia.Interface;
+
+namespace Viv.Entity.CacheBucket
+{
+    public class OneListBucket<T> : ICacheBucket
+    {
+        public OneListBucket() { }
+
+        public List<T> Entities { get; set; }
+
+        public TimeSpan CacheTime => CacheTimeProvider.GetRandomDays();
+
+        public string GetCacheKey(params object[] keys)
+        {
+            if (keys.IsNullOrEmpty()) return string.Empty;
+            return $"OneListBucket_{typeof(T).Name}_{string.Join("_", keys)}";
+        }
+    }
+}
