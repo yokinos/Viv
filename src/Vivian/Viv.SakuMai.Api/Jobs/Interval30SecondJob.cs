406f2480ef055f4e4e978d331b780e0afc1eff97
Leiwanglin <778192960@qq.com>
2026-06-23 14:35:21 +0800

    fix

--- src/Vivian/Viv.SakuMai.Api/Jobs/Interval30SecondJob.cs
+++ src/Vivian/Viv.SakuMai.Api/Jobs/Interval30SecondJob.cs
@@ -4,6 +4,7 @@ using Viv.Delusion;
 using Viv.EventContracts.Apex;
 using Viv.Log;
 using Viv.Nana;
+using Viv.Redis;
 
 namespace Viv.SakuMai.Api.Jobs
 {
@@ -11,16 +12,20 @@ namespace Viv.SakuMai.Api.Jobs
     {
         private readonly ILoggerContract _logger;
         private readonly IVivPublisher _vivPublisher;
+        private readonly IRedisService _redisService;
 
-        public Interval30SecondJob(ILoggerContract logger, IVivPublisher vivPublisher)
+        public Interval30SecondJob(ILoggerContract logger, IVivPublisher vivPublisher, IRedisService redisService)
         {
             _logger = logger;
             _vivPublisher = vivPublisher;
+            _redisService = redisService;
         }
 
         [TickerFunction(nameof(Interval30SecondJob), "*/30 * * * * *")]
         public async Task ExecuteAsync(TickerFunctionContext context, CancellationToken cancellationToken = default)
         {
+            await _redisService.AddAsync("1111", "xxxx");
+
             await _vivPublisher.PublishAsync(new TestApexEvent()
             {
                 IsJob = true,
