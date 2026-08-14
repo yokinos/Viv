334f42a7afbee5df2150821ab94d3ee1243e9373
Leiwanglin <778192960@qq.com>
2026-08-14 11:20:44 +0800

    fix：调整备注

--- src/Banshee/Viv.Contracts/Interface/IDependency.cs
+++ src/Banshee/Viv.Contracts/Interface/IDependency.cs
@@ -5,7 +5,8 @@ using System.Text;
 namespace Viv.Contracts.Interface
 {
     /// <summary>
-    /// 基础依赖注入标记 标记后会自动注入
+    /// 依赖注入标记接口
+    /// 打上该标记的类型，配合 <see cref="Attributes.VivDependencyAttribute"/> 特性，实现自动注册到容器，控制注入生命周期。
     /// </summary>
     public interface IDependency
     {
